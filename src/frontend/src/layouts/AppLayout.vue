9e55198f81a694890b7af463713b37f42b893b0c
xuligan <xuliganmgn@gmail.com>
2022-04-20 21:48:12 +0500

    task 2

---
+++ src/frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <AppLayoutHeader />
+    <slot />
+  </div>
+</template>
+
+<script>
+import AppLayoutHeader from "./AppLayoutHeader.vue";
+export default {
+  name: "AppLayout",
+  components: {
+    AppLayoutHeader,
+  },
+};
+</script>
